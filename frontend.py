b717ca6645e505603372c977d7e63c35957bacaa
ssamuel56 <samkirkendoll@icloud.com>
2025-03-31 03:30:43 -0400

    added a container for the message selection frames so I can style them more easily.

--- frontend.py
+++ frontend.py
@@ -4,10 +4,27 @@ import tkinter
 
 customtkinter.set_appearance_mode("dark")
 
+# Class to create the conversation_selection_frames
+class ConversationFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=2)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=2)
+        self.grid_columnconfigure(2, weight=1)
+
+        self.conversation_name_label = customtkinter.CTkLabel(master=self, fg_color="transparent", font=("Monospace",20))
+        self.conversation_name_label.grid(column=1, columnspan=1, row=2, sticky="")
+
+#        self.configure(border_width=5, border_color="#3C375C")
+
 
 # Class to create the message selection frame
 # Possibly call function in here that creates all the current messages
-class MessageSelectionFrame(customtkinter.CTkScrollableFrame):
+class ConversationSelectionContainer(customtkinter.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
@@ -15,9 +32,10 @@ class MessageSelectionFrame(customtkinter.CTkScrollableFrame):
         # add widgets onto the frame, for example:
         # self.label = customtkinter.CTkLabel(self)
         # self.label.grid(row=0, column=1, padx=20)
-        for x in range(20):
-            self.message_select_button = customtkinter.CTkButton(master=self, text=f"test message {x}", border_width=5)
-            self.message_select_button.pack(pady=(0,5), fill ="x", expand = True)
+        for x in range(2):
+            self.conversation_frame = ConversationFrame(master=self)
+            self.conversation_frame.pack(fill ="x", expand = False, pady=(0, 10))
+            self.conversation_frame.configure(border_width=5, border_color="#3C375C", fg_color="#3c375c")
 
 
 # Class to create the Message content/main area of the app
@@ -34,7 +52,7 @@ class MessagingFrame(customtkinter.CTkFrame):
         #self.grid_rowconfigure(3, weight=3)
         #self.grid_columnconfigure(3, weight=1)
 
-        self.message_title_label = customtkinter.CTkLabel(master=self, text="Peter Peterson", font=("Source Code Pro", 20))
+        self.message_title_label = customtkinter.CTkLabel(master=self, text="Hello", font=("Source Code Pro", 20))
         self.message_title_label.grid(row=0, column = 0, padx=5, pady=5, sticky="new")
 
         self.message_boxes_frame = customtkinter.CTkFrame(master=self, 
@@ -62,8 +80,8 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(2, weight=3)
 
         #Add message selection to app
-        self.message_selection = MessageSelectionFrame(master=self)
-        self.message_selection.grid(row=0, rowspan=2, column=0, padx=(20,0), pady=20, sticky="nsew")
+        self.conversation_selection = ConversationSelectionContainer(master=self)
+        self.conversation_selection.grid(row=0, rowspan=2, column=0, padx=(20,0), pady=20, sticky="nsew")
 
         # Add messages/ main content to app
         self.messaging_frame = MessagingFrame(master=self)
