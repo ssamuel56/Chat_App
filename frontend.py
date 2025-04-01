77bf78b9b5ae39699141be94ac098e759a5c3fd2
ssamuel56 <samkirkendoll@icloud.com>
2025-04-01 04:26:43 -0400

    cleaned up comments and added some todos for studying

--- frontend.py
+++ frontend.py
@@ -4,6 +4,8 @@ from networking import *
 
 customtkinter.set_appearance_mode("dark")
 
+# TODO Need to add function here to keep track of conversations. 
+# Possibly keep connection info in JSON file and use SQLlite for message storage
 # Class to create the conversation_selection_frames
 class ConversationFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -18,8 +20,6 @@ class ConversationFrame(customtkinter.CTkFrame):
 
         self.conversation_name_label = customtkinter.CTkLabel(master=self, fg_color="transparent", font=("Monospace",20))
         self.conversation_name_label.grid(column=1, columnspan=1, row=2, sticky="")
-
-#        self.configure(border_width=5, border_color="#3C375C")
 
 
 # Class to create the message selection frame
@@ -44,12 +44,12 @@ class MessagingFrame(customtkinter.CTkFrame):
         super().__init__(master, **kwargs)
 
         self.grid_rowconfigure(0, weight=0)
-        self.grid_columnconfigure(0,weight=1)
         self.grid_rowconfigure(1, weight=3)
-        #self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=0)
-        #self.grid_columnconfigure(2, weight=1)
         #self.grid_rowconfigure(3, weight=3)
+        self.grid_columnconfigure(0,weight=1)
+        #self.grid_columnconfigure(1, weight=1)
+        #self.grid_columnconfigure(2, weight=1)
         #self.grid_columnconfigure(3, weight=1)
 
         self.message_title_label = customtkinter.CTkLabel(master=self, text="Hello", font=("Source Code Pro", 20))
@@ -63,6 +63,7 @@ class MessagingFrame(customtkinter.CTkFrame):
         self.typed_message_textbox = customtkinter.CTkTextbox(master=self, height=60)
         self.typed_message_textbox.grid(row=2, column=0, padx=5, pady=5, sticky="sew")
 
+    # Used to load in new chat bubbles as new messages appear
     def load_new_message(self, msg):
         self.new_message_bubble = customtkinter.CTkLabel(master=self.message_boxes_frame, text=msg)
         self.new_message_bubble.pack(fill="x", expand=False, pady=5)
@@ -74,7 +75,6 @@ class App(customtkinter.CTk):
         self.geometry("900x600")
         self.title("Geek Chat")
         self.websocket = websocket
-        #self.get_msg = websocket.is_message()
 
         # Configure a grid structure for the application 
         # Goin to use grid for setting all content except for the actual messages and the selection frame
@@ -97,22 +97,20 @@ class App(customtkinter.CTk):
         # self.button = customtkinter.CTkButton(self, command=self.button_click)
         # self.button.grid(row=0, column=4, padx=20, pady=10)
 
-    # add methods to app
+    # This method checks the message queue every second to see if any new messages have arrived.
+    # At that point it creates new chat messages for each one.
+    # TODO This will need to be expanded into another method to allow for multiple conversations.
     def message_checker(self):
-        #print(self.websocket.message_queue.empty())
         while not self.websocket.message_queue.empty():
             print('You Hit Condition')
             self.messaging_frame.load_new_message(self.websocket.message_queue.get())
         self.after(1000, self.message_checker)
 
-    def button_click(self):
-        print("button click")
 
-
+# Run main logic of app and also call our message update function.
+# Must have the first call of .after here or will not run recurssively
 server = WebSocketConnection()
 server.start_as_thread()
-
 app = App(server)
-#app.message_checker()
 app.after(1000, app.message_checker())
 app.mainloop()
